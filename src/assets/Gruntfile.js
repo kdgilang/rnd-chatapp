3172585a046a9d790610faf51d06d3213a2be3d2
kdgilang <gilang.putra@ogilvy.com.au>
2017-11-25 11:42:36 +0800

    INIT ASSETS

---
+++ src/assets/Gruntfile.js
@@ -0,0 +1,38 @@
+'use strict';
+module.exports = function(grunt) {
+	grunt.initConfig({
+		sass: {
+			dist: {
+				options: {
+					style: 'compressed'
+				},
+				files: {
+					'../../app.min.css': ['scss/style.scss', 'scss/bootstrap/bootstrap.scss']
+				}
+			}
+		},
+	    uglify: {
+		    my_target: {
+		      files: {
+	        		'../../app.min.js': ['js/*.js']
+	        	}
+	      	}
+	    },
+		watch: {
+			scss: {
+				files: ['scss/*.scss', 'scss/bootstrap/*.scss'],
+				tasks: ['sass']
+			},
+			scripts: {
+				files: ['js/*.js'],
+				tasks: ['uglify']
+			}
+		}
+	}); 
+
+	grunt.loadNpmTasks('grunt-contrib-sass');
+	grunt.loadNpmTasks('grunt-contrib-uglify');
+	grunt.loadNpmTasks('grunt-contrib-watch');
+
+	grunt.registerTask('default', ['scss', 'scripts']);
+};
